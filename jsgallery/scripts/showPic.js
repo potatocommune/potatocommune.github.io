52ad9036839ee9cf7e9aff01b03954a0fe23f469
Boyang Li <nian1538@gmail.com>
2017-05-22 13:31:24 +0800

    Changes

---
+++ jsgallery/scripts/showPic.js
@@ -0,0 +1,5 @@
+function showPic(whichPic){
+  var source = whichpic.getAttribute("href");
+  var placeholder = document.getElementById("placeholder");
+  placeholder.setAttribute("src", source);
+}
